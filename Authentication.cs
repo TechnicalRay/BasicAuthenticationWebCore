500633aae77704a61a31637dc82fc5fc4360cd1f
kiran potawale <kiranpotawale@gmail.com>
2024-04-01 20:36:59 +0530

    Add project files.

---
+++ Authentication.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace BasicAuthenticationExample
+{
+ 
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+                                          ILoggerFactory logger,
+                                          UrlEncoder encoder,
+                                          ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            try
+            {
+                var authorizationHeader = Request.Headers["Authorization"].ToString();
+                var encodedUsernamePassword = authorizationHeader.Substring("Basic ".Length).Trim();
+                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                var username = decodedUsernamePassword.Split(':')[0];
+                var password = decodedUsernamePassword.Split(':')[1];
+
+                if (IsValidUser(username, password))
+                {
+                    var claims = new[] {
+                        new Claim(ClaimTypes.Name, username),
+                    };
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                    return AuthenticateResult.Success(ticket);
+                }
+                else
+                {
+                    return AuthenticateResult.Fail("Invalid username or password");
+                }
+            }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail("Error occurred during authentication: " + ex.Message);
+            }
+        }
+
+        private bool IsValidUser(string username, string password)
+        {
+            //you can place here your database logic
+            return (username == "TechRay" && password == "TechRay");
+        }
+    }
+
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAuthenticationWebCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Authorize(AuthenticationSchemes = "BasicAuthentication")]
+        [HttpGet]
+        [Route("isAuthenticate")]
+        public IActionResult isAuthenticateAPI()
+        {
+            return Ok("Authentication Successful.!");
+        }
+    }
+}
